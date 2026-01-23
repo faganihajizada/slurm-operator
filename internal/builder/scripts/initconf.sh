09aadd5e235ced4ed44386783aacda8911f95124
Skyler Malinowski <smalinowski@nvidia.com>
2026-01-23 10:22:36 -0500

    fix(initconf): avoid creating SlurmUser by user/group
    There is no need the SlurmUser in the initconf initContainer. We know
    their UID and GID, hence we can set that directly.
    
    Changelog: Fixed - Fixed reliance on adduser and addgroup for initconf
     script, hence any image with basic OS utils should work (e.g. Alpine,
     Ubuntu, Rocky Linux, Alma Linux).

--- internal/builder/scripts/initconf.sh
+++ internal/builder/scripts/initconf.sh
@@ -4,7 +4,6 @@
 
 set -eu
 
-SLURM_USER="slurm"
 SLURM_USER_UID="401"
 SLURM_USER_GID="401"
 SLURM_MOUNT=/mnt/slurm
@@ -13,25 +12,17 @@ SLURM_DIR=/mnt/etc/slurm
 # Workaround to ephemeral volumes not supporting securityContext
 # https://github.com/kubernetes/kubernetes/issues/81089
 
-# Create a Slurm user and group if one does not exist already
-if ! [ "$(getent group ${SLURM_USER})" ]; then
-	addgroup -S -g ${SLURM_USER_GID} ${SLURM_USER}
-fi
-if ! [ "$(getent passwd ${SLURM_USER})" ]; then
-	adduser -S -u ${SLURM_USER_UID} -G ${SLURM_USER} -s /usr/sbin/nologin ${SLURM_USER}
-fi
-
 # Copy Slurm config files, secrets, and scripts
 mkdir -p "$SLURM_DIR"
 find "${SLURM_MOUNT}" -type f -name "*.conf" -print0 | xargs -0r cp -vt "${SLURM_DIR}"
 find "${SLURM_MOUNT}" -type f -name "*.key" -print0 | xargs -0r cp -vt "${SLURM_DIR}"
 
 # Set general permissions and ownership
-find "${SLURM_DIR}" -type f -print0 | xargs -0r chown -v "${SLURM_USER}:${SLURM_USER}"
+find "${SLURM_DIR}" -type f -print0 | xargs -0r chown -v "${SLURM_USER_UID}:${SLURM_USER_GID}"
 find "${SLURM_DIR}" -type f -name "*.conf" -print0 | xargs -0r chmod -v 644
 find "${SLURM_DIR}" -type f -name "slurmdbd.conf" -print0 | xargs -0r chmod -v 600
 find "${SLURM_DIR}" -type f -name "*.key" -print0 | xargs -0r chmod -v 600
-find "${SLURM_DIR}" -type f -name "*.key" -print0 | xargs -0r chown -v "${SLURM_USER}:${SLURM_USER}"
+find "${SLURM_DIR}" -type f -name "*.key" -print0 | xargs -0r chown -v "${SLURM_USER_UID}:${SLURM_USER_GID}"
 
 # Display Slurm directory files
 ls -lAF "${SLURM_DIR}"
