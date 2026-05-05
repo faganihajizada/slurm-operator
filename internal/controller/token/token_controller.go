c641fe3f68720f9cf8a921e112d8081d72e47720
Alan Mutschelknaus <amutschelkna@nvidia.com>
2026-05-05 13:51:27 -0500

    fix: pass token-controller token-workers flag when initializing
    Changelog: Fixed - Fixed token-controller not passing --token-workers
     when initializing the controller.

--- internal/controller/token/token_controller.go
+++ internal/controller/token/token_controller.go
@@ -16,6 +16,7 @@ import (
 	"k8s.io/client-go/util/flowcontrol"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
@@ -99,6 +100,9 @@ func (r *TokenReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&slinkyv1beta1.Token{}).
 		Owns(&corev1.Secret{}).
+		WithOptions(controller.Options{
+			MaxConcurrentReconciles: maxConcurrentReconciles,
+		}).
 		Complete(r)
 }
 
