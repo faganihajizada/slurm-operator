28472d9f44860e2d2ef6e373772350ec0eaa8fe8
Skyler Malinowski <smalinowski@nvidia.com>
2026-06-10 13:42:57 -0400

    refactor: condense logic into makePodCordonAndDrain
    Updates the default drain reason. Should no callers provide a reason,
    one is still given to avoid a Slurm error. Unlike undrain requests,
    Slurm require a non-empty reason message for drain requests.

--- internal/controller/nodeset/nodeset_sync.go
+++ internal/controller/nodeset/nodeset_sync.go
@@ -1386,23 +1386,8 @@ func (r *NodeSetReconciler) makePodCordonAndDrain(
 		return err
 	}
 
-	if err := r.syncSlurmNodeDrain(ctx, nodeset, pod, reason); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// syncSlurmNodeDrain will drain the corresponding Slurm node.
-func (r *NodeSetReconciler) syncSlurmNodeDrain(
-	ctx context.Context,
-	nodeset *slinkyv1beta1.NodeSet,
-	pod *corev1.Pod,
-	message string,
-) error {
-	reason := fmt.Sprintf("Pod (%s) has been cordoned", klog.KObj(pod))
-	if message != "" {
-		reason = message
+	if reason == "" {
+		reason = "unknown"
 	}
 
 	if err := r.slurmControl.MakeNodeDrain(ctx, nodeset, pod, reason); err != nil {
